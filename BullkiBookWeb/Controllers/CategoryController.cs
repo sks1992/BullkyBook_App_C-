8d102d665840fa8d8171a4fe7156750d2874232d
Sandeep <sandeepsk15892>
2023-02-23 17:13:55 +0530

    code

---
+++ BullkiBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BullkyBookWeb.Data;
+using BullkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+                _db= db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Category;
+            return View(objCategoryList);
+        }
+    }
+}
+    
