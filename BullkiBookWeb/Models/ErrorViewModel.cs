4eee93ef478d4361fa2bfd41cb33c7543817f04f
Sandeep <sandeepsk15892>
2023-02-23 10:51:32 +0530

    Add project files.

---
+++ BullkiBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BullkiBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
